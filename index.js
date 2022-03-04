cf628f5f83842336f682a5418fbffa6a81cfb187
Rick Willcox <bigshakagames@gmail.com>
2022-03-04 13:18:19 +1000

    mongo connecting to express

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 const app = express();
+
+mongoose
+    .connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@mongo:27017/?authSource=admin`) //mongo is replaced by IP as custom networks can talk to each other by name
+    .then(() => {
+        console.log('successfully connected to mongo db!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.get('/', (req, res) => {
     res.send('<h2> Hello Worl1d!</h2>');
